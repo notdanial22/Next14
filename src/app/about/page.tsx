4e68ff7c7ad21c616bcd234b2aee6cc3833cb0c8
notdanial22 <notdanial22@gmail.com>
2024-03-27 21:47:47 +0500

    updated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,10 @@
+import Image from 'next/image'
+import styles from './about.module.css'
+
 export default function aboutPage() {
-  return <div>About page</div>
+  return (
+    <div className={styles.imgContainer}>
+      <Image src='/about.png' alt='' fill></Image>
+    </div>
+  )
 }
